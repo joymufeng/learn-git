28b0cdfd39662fd9791f8f6a54d6ef9d81d027a6
dev01 <dev01@gmail.com>
2019-04-23 16:36:10 +0800

    Edit line1.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, 2");
+        System.out.println("Hello, line1");
         System.out.println("Commit form dev02.");
         System.out.println("Hello, Git!");
         System.out.println("Commit from Dev01.");
